bb791e81483c1a681f379a67389856c8ed78f2c6
jsjiang <js4jiang5@users.noreply.github.com>
2026-05-16 17:24:11 +0800

    v0.0.1

--- Casks/andish.rb
+++ Casks/andish.rb
@@ -1,6 +1,6 @@
 cask "andish" do
-  version "0.0.2"
-  sha256 "59bb7708c34cf7a76380cb35c59806e82f1aa1a3cd1a73fab70488cd4346419f"
+  version "0.0.1"
+  sha256 "6a41b583bcf309a041643a928df9e3778510c0bda8ebb15045c92d8de4c40457"
 
   url "https://github.com/js4jiang5/Andish/releases/download/v#{version}/Andish_v#{version}.dmg"
   name "Andish"
