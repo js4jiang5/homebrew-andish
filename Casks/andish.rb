bd7036204d2a49def4dfc7be0e05d1a4a7c60333
jsjiang <js4jiang5@users.noreply.github.com>
2026-05-16 16:47:53 +0800

    - v0.0.1

---
+++ Casks/andish.rb
@@ -0,0 +1,36 @@
+cask "andish" do
+  version "0.0.1"
+  sha256 "6a41b583bcf309a041643a928df9e3778510c0bda8ebb15045c92d8de4c40457"
+
+  url "https://github.com/js4jiang5/Andish/releases/download/v#{version}/Andish_v#{version}.dmg"
+  name "Andish"
+  desc "Monitor Android device battery health, cycle, and temperature information."
+  homepage "https://github.com/js4jiang5/Andish"
+
+  app "Andish.app"
+
+  # This runs AFTER the app is moved to /Applications
+  postflight do
+    system_command "/usr/bin/xattr",
+                   args: ["-rd", "com.apple.quarantine", "#{appdir}/Andish.app"],
+                   sudo: false
+  end
+
+  uninstall_preflight do
+    current_uid = Process.uid
+    system_command "/bin/launchctl",
+                   args: ["bootout", "gui/#{current_uid}", "#{appdir}/Andish.app/Contents/Library/LaunchAgents/com.buddha-path.Andish.daemon.plist"],
+                   must_succeed: false
+
+    system_command "/usr/bin/pkill", 
+                   args: ["-9", "-x", "Andish"], 
+                   must_succeed: false
+  end
+
+  zap trash: [
+        "~/Library/Application Support/andish/andish.sock",
+        "~/Library/Logs/andish/andish.log",
+        "~/Library/Caches/com.buddha-path.Andish",
+        "~/Library/Preferences/com.buddha-path.Andish.plist",
+      ]
+end
